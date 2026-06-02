6c749df643b2bb732fe7025df75ffc2ef1f2c4a9
SkyLuckee <locnd07@gmail.com>
2026-06-02 16:38:36 +0700

    feat: methods and array Im so sigma

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,1 +1,70 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+// Array aka a list
+// Declared using a type followed by square brackets
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+int[] myNum = { 10, 20, 30, 40 };
+
+// Accessing the elements of an array using index
+Console.WriteLine(cars[0]); // Outputs "Volvo"
+
+// Modifying an element of an array
+cars[0] = "Opel";
+
+// Array length
+Console.WriteLine(cars.Length);
+
+// other way to declare an array
+// Create an array of four elements, and add values later
+string[] cars1 = new string[4];
+
+// Create an array of four elements and add values right away 
+string[] cars2 = new string[4] { "Volvo", "BMW", "Ford", "Mazda" };
+
+// Create an array of four elements without specifying the size 
+string[] cars3 = new string[] { "Volvo", "BMW", "Ford", "Mazda" };
+
+// Create an array of four elements, omitting the new keyword, and without specifying the size
+string[] cars4 = { "Volvo", "BMW", "Ford", "Mazda" };
+
+//declaring array and then initializing it later
+string[] cars5;
+cars5 = new string[] { "Volvo", "BMW", "Ford", "Mazda" }; // have to use new keyword when initializing an array after declaration
+
+// Array methods
+// Sorts the elements of an array in alphabetical order or ascending order
+Array.Sort(cars);
+foreach (string i in cars)
+{
+    Console.WriteLine(i);
+}
+
+Array.Sort(myNum);
+foreach (int i in myNum)
+{
+    Console.WriteLine(i);
+}
+
+// Other useful array methods, such as Min, Max, and Sum, can be found in the System.Linq namespace
+//using System;
+//using System.Linq;
+int[] myNumbers = { 5, 1, 8, 9 };
+Console.WriteLine(myNumbers.Max());  // returns the largest value
+Console.WriteLine(myNumbers.Min());  // returns the smallest value
+Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
+
+// Multidimensional arrays
+int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } }; // [,] -> 2D array, [,,] -> 3D array, etc.
+
+// accessing elements of a multidimensional array
+Console.WriteLine(numbers[0,2]); // Output: 2
+
+numbers[0, 0] = 5;  // Change value to 5
+Console.WriteLine(numbers[0, 0]); // Outputs 5 instead of 1
+
+// looping through a multidimensional array
+foreach (int i in numbers)
+{
+    Console.WriteLine(i);
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿// Methods / functions
+
+static void MyMethod() // defined by name + ()
+{
+    // static means that the method belongs to the Program class and not an object of the Program class
+    // normally, methods are tied to objects so the static keyword is used to create methods that are not tied to objects
+
+    // void means that this method does not have a return value
+    Console.WriteLine("Hello");
+}
+MyMethod(); // calling the method by its name + ()
+
+// Information can be passed to methods as parameter.
+static void Name(string fname, int age)
+{
+    Console.WriteLine(fname + " Refsnes" + age);
+}
+Name("Liam",21);
+
+// Default parameter values
+static void Nation(string country = "Norway") // if no argument is provided, the default value will be used
+{
+    Console.WriteLine(country);
+}
+Nation(); // will print "Norway")
+
+// Return values from methods
+static int Sum(int x) // replace the void keyword with the type of value you want to return (in this case int)
+{
+    return 5 + x;
+}
+var z = Sum(6); // will return 11
+Console.WriteLine(z);
+
+// Method overloading
+// With method overloading, multiple methods can have the same name with different parameters
+//static int PlusMethod(int x, int y)
+//{
+//    return x + y;
+//}
+//static double PlusMethod(double x, double y)
+//{
+//    return x + y;
+//}
+
+//int myNum1 = PlusMethod(8, 5);
+//double myNum2 = PlusMethod(4.3, 6.26);
+
+//Console.WriteLine("Int: " + myNum1);
+//Console.WriteLine("Double: " + myNum2);
